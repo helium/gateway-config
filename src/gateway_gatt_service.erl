0f4d7557afaf6c382d373c53fca73ceb83215980
Marc Nijdam <marc@helium.com>
2018-11-19 16:37:46 -0600

    Enable wifi on gatt_service startup

--- src/gateway_gatt_service.erl
+++ src/gateway_gatt_service.erl
@@ -22,8 +22,12 @@ init(_) ->
          {gateway_gatt_char_wifi_ssid, 1, []},
          {gateway_gatt_char_wifi_pass, 2, []}
         ],
+    self() ! enable_wifi,
     {ok, Characteristics, #state{}}.
 
+handle_info(enable_wifi, State=#state{}) ->
+    connman:enable(wifi, true),
+    {noreply, State};
 handle_info({changed_wifi_ssid, Value}, State=#state{}) ->
     {noreply, State#state{ssid=binary_to_list(Value)}};
 handle_info({changed_wifi_pass, _}, State=#state{ssid=""}) ->
