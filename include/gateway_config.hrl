489e816d15e77f79d71d5eab714b8de9adc0b0d0
Marc Nijdam <marc.nijdam@gmail.com>
2019-01-05 14:42:27 -0600

    ACK when QR Code Submitted to Wallet (#9)
    * Add initial suppor for miner add_gateway_status signals
    
    * Add validation to qr code status setting

--- include/gateway_config.hrl
+++ include/gateway_config.hrl
@@ -1,6 +1,6 @@
 -define(CONFIG_APPLICATION_NAME, "com.helium.Config").
 
--define(CONFIG_OBJECT_PATH, "/com/helium/Config").
+-define(CONFIG_OBJECT_PATH, "/").
 -define(CONFIG_OBJECT_INTERFACE, "com.helium.Config").
 -define(CONFIG_OBJECT(M), ?CONFIG_OBJECT_INTERFACE ++ "." ++ M).
 
@@ -8,3 +8,15 @@
 -define(CONFIG_MEMBER_POSITION_LOCK, "PositionLock").
 -define(CONFIG_MEMBER_ADD_GW, "AddGateway").
 -define(CONFIG_MEMBER_DOWNLOADING, "Downloading").
+
+-define(MINER_OBJECT_PATH, "/").
+-define(MINER_INTERFACE, "com.helium.Miner").
+-define(MINER_OBJECT(M), ?MINER_INTERFACE ++ "." ++ M).
+
+%% Values for AddGatewayStatus are:
+%%   * "init" - Initial (startup) state
+%%   * "received" - Add gateway request (QRCode) received by hotspot
+%%   * "sending" - Add gateway request is being sent to wallet
+%%   * "sent" - Add gateway request successfully submitted to wallet
+%%   * "send_failed" - Failed to send add gateway request to wallet
+-define(MINER_MEMBER_ADD_GW_STATUS, "AddGatewayStatus").
