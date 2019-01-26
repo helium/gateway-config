538c755ad48db01ee948c9f65844235446f7de4a
Frank Vasquez <frankv@helium.com>
2019-01-26 12:07:51 -0800

    revert back to info nav_timeutc

--- src/gateway_config_worker.erl
+++ src/gateway_config_worker.erl
@@ -110,7 +110,7 @@ set_system_time(UbxPid) ->
         {ok, {nav_timeutc, #{datetime := {{Year, Month, Day}, {Hour, Min, Sec}}}}} ->
             Ymd = io_lib:format("~b-~2..0b-~2..0b", [Year, Month, Day]),
             Hms = io_lib:format("~2..0b:~2..0b:~2..0b", [Hour, Min, Sec]),
-            lager:debug("Setting date to ~p ~p", [Ymd, Hms]),
+            lager:info("Setting date to ~p ~p", [Ymd, Hms]),
             os:cmd("date -s '" ++ Ymd ++ " " ++ Hms ++ "'");
         _ ->
             lager:warning("No valid UTC datetime found")
