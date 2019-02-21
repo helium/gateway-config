f1940516052a1a8b48c6ffc0c55f6df5cb2ce5de
Marc Nijdam <marc@helium.com>
2019-02-20 18:10:39 -0800

    Pass calculated H3 index as a string to miner

--- src/gateway_gatt_char_assert_loc.erl
+++ src/gateway_gatt_char_assert_loc.erl
@@ -44,9 +44,12 @@ write_value(State=#state{}, Bin) ->
     try gateway_gatt_char_assert_loc_pb:decode_msg(Bin, gateway_assert_loc_v1_pb) of
         #gateway_assert_loc_v1_pb{lat=Lat, lon=Lon, owner=Owner, nonce=Nonce, fee=Fee} ->
             H3Index = h3:from_geo({Lat, Lon}, ?H3_LATLON_RESOLUTION),
+            H3String = h3:to_string(H3Index),
+            lager:info("Requesting assert_loc_txn for lat/lon/acc: {~p, ~p, ~p} index: ~p",
+                       [Lat, Lon, ?H3_LATLON_RESOLUTION, H3String]),
             Value = case ebus_proxy:call(State#state.proxy, "/", ?MINER_OBJECT(?MINER_MEMBER_ASSERT_LOC),
-                                         [uint64, string, uint64, uint64],
-                                         [H3Index, Owner, Nonce, Fee]) of
+                                         [string, string, uint64, uint64],
+                                         [H3String, Owner, Nonce, Fee]) of
                         {ok, [BinTxn]} ->  BinTxn;
                         {error, Error} ->
                             lager:warning("Failed to get assert_loc txn: ~p", [Error]),
